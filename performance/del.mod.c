60d45cc2ca16b2e15a36303da557935f8136b885
jubayer0175 <jubayer@vt.edu>
2020-12-10 21:14:32 -0500

    bench

---
+++ performance/del.mod.c
@@ -0,0 +1,36 @@
+#include <linux/module.h>
+#include <linux/vermagic.h>
+#include <linux/compiler.h>
+
+MODULE_INFO(vermagic, VERMAGIC_STRING);
+MODULE_INFO(name, KBUILD_MODNAME);
+
+__visible struct module __this_module
+__attribute__((section(".gnu.linkonce.this_module"))) = {
+	.name = KBUILD_MODNAME,
+	.init = init_module,
+#ifdef CONFIG_MODULE_UNLOAD
+	.exit = cleanup_module,
+#endif
+	.arch = MODULE_ARCH_INIT,
+};
+
+static const struct modversion_info ____versions[]
+__used
+__attribute__((section("__versions"))) = {
+	{ 0xcfa188e7, __VMLINUX_SYMBOL_STR(module_layout) },
+	{ 0x37a0cba, __VMLINUX_SYMBOL_STR(kfree) },
+	{ 0xa5ef469c, __VMLINUX_SYMBOL_STR(kmem_cache_alloc_trace) },
+	{ 0x25a0520a, __VMLINUX_SYMBOL_STR(kmalloc_caches) },
+	{ 0x27e1a049, __VMLINUX_SYMBOL_STR(printk) },
+	{ 0xbdfb6dbb, __VMLINUX_SYMBOL_STR(__fentry__) },
+	{ 0x8ff4079b, __VMLINUX_SYMBOL_STR(pv_irq_ops) },
+};
+
+static const char __module_depends[]
+__used
+__attribute__((section(".modinfo"))) =
+"depends=";
+
+
+MODULE_INFO(srcversion, "DA9F24037B968C91AD5B641");
